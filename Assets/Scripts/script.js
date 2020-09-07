1f114db3893cd770f0885b584d14d283f02977d4
ad-fleming <adfleming@gmail.com>
2020-09-07 11:54:54 -0400

    created a function to set the game timer and triggered that function on a click event of the beginQuizBtn

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,5 +1,30 @@
-var navEl = document.querySelector(".navEl");
-var highScoreElement = document.getElementById("highScoreElement");
-var timerElement = document.getElementById("timerElement");
+// Define elements and console.log to make sure
+var highScoreEl = document.getElementById("highScoreEl");
+var timerEl = document.getElementById("timerEl");
+var cardContainerEl = document.getElementById("cardEl");
+var pageTitleEl = document.getElementById("pageTitleEl");
+var questionDisplayEl = document.getElementById("questionDisplayEl");
+var beginQuizBtn = document.getElementById("beginQuizBtn");
+var answerBtnEl = document.getElementsByClassName("answerBtn");
+var lineEl = document.getElementById("lineId");
+var resultText = document.getElementById("resultText");
+var timerCount = 10;
 
-timerElement.textContent = "Time: 45" 
+
+beginQuizBtn.addEventListener("click", function(event){
+    setTimer();
+
+});
+
+function setTimer(){
+timeInterval = setInterval(() => {
+    timerCount--;
+
+if(timerCount === 0){
+    clearInterval(timeInterval);
+    timerEl.textContent = "Time's up!"
+}else{
+    timerEl.textContent = "Time: " + timerCount
+}
+}, 1000);
+}
