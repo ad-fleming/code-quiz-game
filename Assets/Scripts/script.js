efdb9949298e11de4de4d4ddb55b05afccda79cf
ad-fleming <adfleming@gmail.com>
2020-09-07 13:06:42 -0400

    created the first rough function for pageChange() to change values of, title question and buttons. Would like to create a randomizer to control which answers are displayed on which buttons

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -5,17 +5,58 @@ var cardContainerEl = document.getElementById("cardEl");
 var pageTitleEl = document.getElementById("pageTitleEl");
 var questionDisplayEl = document.getElementById("questionDisplayEl");
 var beginQuizBtn = document.getElementById("beginQuizBtn");
-var answerBtnEl = document.getElementsByClassName("answerBtn");
+var potentialAnswer = document.getElementsByClassName("potentialAnswer");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
 var lineEl = document.getElementById("lineId");
 var resultText = document.getElementById("resultText");
-var timerCount = 10;
+var timerCount = 100;
+var currentPage = 0;
 
+var gamePageArray = [
+    {
+        page: 0
+    },
+    {
+      page: 1,
+      title: "Question 1",
+      question: "Is this question 1?",
+      answers: ["answer 1", "answer 2", "answer 3"],
+      correctAnswer: "correct answer"  
+    },
+    {
+        page: 2,
+        title: "Question 2",
+        question: "Is this question 2?",
+        answers: ["answer 1", "answer 2", "answer 3"],
+        correctAnswer: "correct answer"  
+    },
+    {
+        page: 3,
+        title: "Question 3",
+        question: "Is this question 3?",
+        answers: ["answer 1", "answer 2", "answer 3"],
+        correctAnswer: "correct answer"  
+    },
+    {
+        page: 4,
+        title: "Question 4",
+        question: "Is this question 4?",
+        answers: ["answer 1", "answer 2", "answer 3"],
+        correctAnswer: "correct answer"  
+    }
+]
 
+// Add event listener to the beginQuizBtn to start timer
 beginQuizBtn.addEventListener("click", function(event){
     setTimer();
-
+    pageChange();
+    
 });
 
+// Define a setTimer function which is triggered on a click event of the beginQuizBtn
 function setTimer(){
 timeInterval = setInterval(() => {
     timerCount--;
@@ -28,3 +69,22 @@ if(timerCount === 0){
 }
 }, 1000);
 }
+
+function pageChange () {
+    currentPage ++;
+    beginQuizBtn.classList.add("hideMe");
+    answer1.classList.remove("hideMe");
+    answer2.classList.remove("hideMe");
+    answer3.classList.remove("hideMe");
+    answer4.classList.remove("hideMe");
+    pageTitleEl.textContent = gamePageArray[currentPage].title;
+    questionDisplayEl.textContent = gamePageArray[currentPage].question;
+
+    // I want to build a randomizer here
+    answer1.textContent = gamePageArray[currentPage].answers[2];
+    answer2.textContent = gamePageArray[currentPage].answers[0];
+    answer3.textContent = gamePageArray[currentPage].correctAnswer;
+    answer4.textContent = gamePageArray[currentPage].answers[1];
+    
+}
+
