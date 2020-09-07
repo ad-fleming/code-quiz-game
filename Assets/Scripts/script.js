7cd9b00aa1e5ab51fd5b6242f685abe4912f9a88
ad-fleming <adfleming@gmail.com>
2020-09-07 11:19:11 -0400

    styled landing page and set buttons to hide

--- Assets/Scripts/script.js
+++ Assets/Scripts/script.js
@@ -1,1 +1,5 @@
-console.log("Hello World!")
+var navEl = document.querySelector(".navEl");
+var highScoreElement = document.getElementById("highScoreElement");
+var timerElement = document.getElementById("timerElement");
+
+timerElement.textContent = "Time: 45" 
